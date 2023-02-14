5424e7d2930e9fdb800020579c1eca2dcc55ba85
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-02-13 22:08:38 -0800

    added api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8800',
+      target: 'https://booknow-com-api.onrender.com',
       changeOrigin: true,
     })
   );
