61bb6513f8f157db4ef9b24051f8eb905e266ca5
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-06-22 16:29:31 -0700

    added lazy loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
+import React, { Suspense } from 'react'
+// import Featured from '../../components/featured/Featured'
 // import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
 // import MailList from '../../components/mailList/MailList'
 import Navbar from '../../components/navbar/Navbar'
-import PropertyList from '../../components/propertyList/PropertyList'
+// import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
+const Featured = React.lazy(() => import('../../components/featured/Featured'))
+const PropertyList = React.lazy(() => import('../../components/propertyList/PropertyList'));
 const MailList = React.lazy(() => import('../../components/mailList/MailList'));
 const FeaturedProperties = React.lazy(() => import('../../components/featuredProperties/FeaturedProperties'));
 
@@ -14,16 +16,18 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <Header />     
-      <div className="homeContainer">
-        <Featured />
-        <h1 className='homeTitle'>Browse by property type</h1>
-        <PropertyList />
-        <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties />
-        <MailList />
-        <Footer />
-      </div> 
+      <Header />   
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="homeContainer">
+          <Featured />
+          <h1 className='homeTitle'>Browse by property type</h1>
+          <PropertyList />
+          <h1 className="homeTitle">Homes guests love</h1>
+          <FeaturedProperties />
+          <MailList />
+          <Footer />
+        </div> 
+      </Suspense>        
     </div>
     
   )
